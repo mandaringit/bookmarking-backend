883acb655f6b47e195b83f3feb0002d140974199
mandaringit <mandarin.dev@kakao.com>
2020-12-09 22:03:31 +0900

    add: cors 추가

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import { createConnection } from "typeorm";
 import express from "express";
 import logger from "morgan";
 import todosRoutes from "./routes/todos";
+import cors from "cors";
 
 createConnection()
   .then(async (connection) => {
     const app = express();
+    app.use(cors({ origin: "http://localhost:3000", credentials: true }));
     app.use(express.json());
     app.use(logger("dev"));
 
