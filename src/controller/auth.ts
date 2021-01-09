ebd212f12997792c903f67d8a37dfc57cadce28e
mandaringit <mandarin.dev@kakao.com>
2021-01-09 09:14:37 +0900

    add: 로그아웃 라우트 추가

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -2,15 +2,17 @@ import { RequestHandler } from "express";
 import { User } from "../entity/User";
 import "../passport";
 
+export const logout: RequestHandler = (req, res, next) => {
+  req.logout();
+  return res.status(200).send({ message: "로그아웃 성공" });
+};
+
 /**
  * 로컬 로그인 성공 요청시 핸들러. 실패는 패스포트 미들웨어쪽에서 401로 처리됨.
- * @param req
- * @param res
- * @param next
  */
 export const login: RequestHandler = (req, res, next) => {
   const { id, email, googleId, username } = req.user as User;
-  res.status(200).send({
+  return res.status(200).send({
     id,
     email,
     googleId,
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import passport from "passport";
-import { login } from "../controller/auth";
+import { login, logout } from "../controller/auth";
 import { isAuthenticated } from "../middlewares/auth";
 
 const router = Router();
 
+router.get("/logout", isAuthenticated, logout);
 router.get("/checkAuth", isAuthenticated, login);
 router.post("/local", passport.authenticate("local"), login);
 // router.get(
