67f860429afae0300adb3323332d868e01a24614
mandaringit <mandarin.dev@kakao.com>
2021-01-27 21:18:27 +0900

    fix: CORS 테스트

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,12 @@ dotenv.config();
 createConnection()
   .then(async (connection) => {
     const app = express();
-    app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+    app.use(
+      cors({
+        origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+        credentials: true,
+      })
+    );
     app.use(express.json());
     app.use(logger("dev"));
 
