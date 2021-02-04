eccc884455a92320d77f93a5e8eb0ef765c9368e
mandaringit <mandarin.dev@kakao.com>
2021-02-04 20:34:21 +0900

    fix: hasbook을 못찾는 현상

--- src/lib/library.ts
+++ src/lib/library.ts
@@ -15,26 +15,31 @@ export interface LibaryResponse {
 }
 
 export async function getLibraryStatus(libCode: string, isbn13: string) {
-  const { data: xml } = await axios.get(
-    "http://data4library.kr/api/bookExist",
-    {
-      params: {
-        authKey: process.env.LIBRARY_AUTH_KEY,
-        libCode,
-        isbn13,
-      },
-    }
-  );
+  try {
+    const { data: xml } = await axios.get(
+      "http://data4library.kr/api/bookExist",
+      {
+        params: {
+          authKey: process.env.LIBRARY_AUTH_KEY,
+          libCode,
+          isbn13,
+        },
+      }
+    );
+    console.log(xml);
+    // xml -> js 변환
+    const jsConverted = convert.xml2js(xml, {
+      compact: true,
+      ignoreDeclaration: true,
+    });
 
-  // xml -> js 변환
-  const jsConverted = convert.xml2js(xml, {
-    compact: true,
-    ignoreDeclaration: true,
-  });
+    const { response } = jsConverted as LibaryResponse;
+    console.log(response);
+    const hasBook = response.result.hasBook._text;
+    const loanAvailable = response.result.loanAvailable._text;
 
-  const { response } = jsConverted as LibaryResponse;
-  const hasBook = response.result.hasBook._text;
-  const loanAvailable = response.result.loanAvailable._text;
-
-  return { hasBook, loanAvailable };
+    return { hasBook, loanAvailable };
+  } catch (e) {
+    console.log(e);
+  }
 }
